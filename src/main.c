4328b96fd6160a85575c0c68cf50825249057316
dafonsoge1 <44798224+dafonsoge1@users.noreply.github.com>
2018-11-15 11:14:59 +0100

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -2,8 +2,28 @@
 //
 
 #include "my_header.h"
+#include "TP_lib.h"
 
 int main(void)
 {
+	char lancer = 0, score = 0;
+	while (lancer != 1)
+	{	
+		lancer = LancerDe();
+		if (lancer == 2 || lancer == 4 || lancer == 6)
+		{
+			score = lancer + score;
+		}
+		if (lancer == 3)
+		{
+			score = score * 2;
+		}
+		if (lancer == 5 && score > 2)
+		{
+			score = score - 2;
+		}	
+	}	
+	
+	
     return 0;
 }
