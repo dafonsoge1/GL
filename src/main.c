3ef8b342f9e01caab4c14ba2a0a487fd65423122
dylan afonso <dylan.afonso@ge1.iut-cachan.u-psud.fr>
2018-11-20 09:48:21 +0100

    je démarre exo56

--- src/main.c
+++ src/main.c
@@ -2,8 +2,13 @@
 //
 
 #include "my_header.h"
+#define SEUIL 19
 
 int main(void)
 {
+	unsigned char bin [10] = {10,15,14,18,20,25,16,23,22,28};
+	
+	for (bin = 0; bin<SEUIl; bin++)
+		if(
     return 0;
 }
